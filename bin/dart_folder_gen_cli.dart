2f2eec54296b3005a479cc27469253782c8701c5
Mehmet Karanlık <89414084+MehmetKaranlik@users.noreply.github.com>
2022-09-07 13:39:21 +0300

    remove unnesecaart things

--- bin/dart_folder_gen_cli.dart
+++ bin/dart_folder_gen_cli.dart
@@ -7,5 +7,5 @@ void main(List<String> arguments) async {
   Prompt(Messages.descriptionSecondary).printOut();
   final String? folderPath = stdin.readLineSync();
 
-  await ModuleManager(path: folderPath ?? '', moduleName: moduleName ?? '').createModule();
+  await ModuleManager(path: folderPath, moduleName: moduleName ?? '').createModule();
 }
