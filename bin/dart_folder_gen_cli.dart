78f6ae116b2a541dc745814e9c1c534d77943b55
Mehmet Karanlık <89414084+MehmetKaranlik@users.noreply.github.com>
2022-09-07 04:47:42 +0300

    Init project

---
+++ bin/dart_folder_gen_cli.dart
@@ -0,0 +1,5 @@
+import 'package:dart_folder_gen_cli/dart_folder_gen_cli.dart' as dart_folder_gen_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_folder_gen_cli.calculate()}!');
+}
---
+++ test/dart_folder_gen_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_folder_gen_cli/dart_folder_gen_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
